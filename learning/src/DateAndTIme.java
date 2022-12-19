9c80fec00b51e598c677d428e1f1497acff2d244
andrei_pirin <satvic@inbox.lv>
2022-12-19 16:44:03 +0200

    !

---
+++ learning/src/DateAndTIme.java
@@ -0,0 +1,12 @@
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DateAndTIme {
+
+    public static void main(String[] args) {
+        java.text.DateFormat format = new SimpleDateFormat("HH:mm dd.MM.yyyy");
+        Date date = new Date();
+        System.out.println(format.format(date));
+    }
+}
